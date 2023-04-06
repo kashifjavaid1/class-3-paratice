391a41bcf634bf4e00dab378daa165f65a588532
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-06 23:00:07 +0500

    variable create

---
+++ script.js
@@ -0,0 +1,153 @@
+// var name=5;
+// console.log("kashif",+name);
+// var name1="kashif";
+// console.log("Muhammad"+name1);
+// var int=5;
+// console.log("2"+int)
+
+// artihmatic Operatores
+
+// var int=21;
+// var int1=55;
+// var result=int+int1;
+// console.log(result);
+
+// var sub=50;
+// var sub1=20;
+// console.log(sub-sub1);
+// console.log(sub*sub1);
+// console.log(sub/sub1);
+
+// Assignment Operators
+// var x=5;
+// var y=4;
+// y++;
+// var x= x+y;
+// console.log(x);
+
+// var a=10.34;
+// var b=120;
+// var c=15;
+// var a=a+b;
+// console.log(a);
+//  var c=205;
+//  var d=150;
+//  var c=c-d;
+//  console.log(c);
+//  var a=5;
+//  var b=20;
+//  var a=a+b;
+//  console.log(a);
+// var a='5';
+// a=parseInt(a);
+// console.log("result=>",a, typeof(a));
+
+//  comparion operatores
+//  < less then 
+// var num=52;
+// var num1=12;
+//  console.log(num<num1);
+// var a=10;
+// var b=5;
+// console.log(a<b);
+// var a=20;
+// var b=25;
+// console.log(a<b);
+// var c=15.5;
+// var d=19;
+// console.log(c<d);
+// var a=50;
+// var b=60;
+// console.log(a<b);
+// var c=10.5;
+// var d=2;
+// console.log(c<d);
+
+// > Greater then
+//    var a=20;
+//    var b=10;
+//    console.log(a>b);
+ 
+// var name=50;
+// var name1=60;
+// console.log(name > name1);
+// var a=40;
+// var b=10;
+// console.log(a>b);
+
+// var c=30;
+// var d=20;
+// console.log(c==d);
+// var a=10;
+// var b=12;
+// console.log(a!=b);
+
+// program
+// var userinput=+prompt("User Enter number");
+// var userinput1=+prompt("User Enter a number");
+//     if (userinput+userinput1 ||userinput-userinput1) {
+//     console.log("Result",userinput+userinput1);
+// }
+// else if (userinput-userinput1) {
+//  console.log('Result',userinput-userinput1);
+// }
+
+// function abc(number) {
+//     console.log("paksitan")
+// }
+// abc()
+// function sumtwonumber(a,b) {
+//     // var a=5;
+//     // var b=1;
+//     var a=prompt("enter number");
+// var b=prompt("enter number");
+//     console.log(a+b);
+// }
+
+// sumtwonumber(a+b)
+
+// function addtwonumber(userinput,userinput1) {
+//     var result=+userinput+userinput1;
+//     console.log("result",result);
+// }
+// var userinput=+prompt("Entet Number");
+// var userinput1=+prompt("Enter Number");
+// addtwonumber(userinput,userinput1)
+
+// function abc(a,b){
+//     var sum1=+prompt("Enter User Number");
+//     var sum2=+prompt("Enter User Number");
+//     var result=sum1+sum2;
+//     console.log("result",result);
+// }
+// abc()
+// function abc(a,b){
+//     var age=+prompt("Enter user age")
+//     if (age>=1 && age<=15 ) {
+//         alert("Not Apply this job");
+//     }
+//     else if (age >=16 && age<=23)
+//     {
+//         alert("Apply this job");
+//     }
+//     else 
+//     {
+//         alert("Over age this job");
+//     }
+// }
+// abc()
+
+function abc(sum,sum1) {
+    var num=+prompt("Enter a Number");
+    var num1=+prompt("Enter a Number"); 
+    var result=num==num1;
+    if (num==num1) {
+        console.log("Both Number a equal",result);
+    }
+    else{
+        console.log("Both Number Not a equal",result);
+    }
+}
+abc()
+
+
