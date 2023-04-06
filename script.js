383823255e3fc5acb74ebc01987f64fba6efce8f
kashifjavaid1 <gmdcf797@gmail.com>
2023-04-06 23:04:15 +0500

    Function paractice

--- script.js
+++ script.js
@@ -64,84 +64,84 @@
 // console.log("result=>",a, typeof(a));
 
 //  comparion operatores
- < less then 
-var num=52;
-var num1=12;
- console.log(num<num1);
-var a=10;
-var b=5;
-console.log(a<b);
-var a=20;
-var b=25;
-console.log(a<b);
-var c=15.5;
-var d=19;
-console.log(c<d);
-var a=50;
-var b=60;
-console.log(a<b);
-var c=10.5;
-var d=2;
-console.log(c<d);
-
-// > Greater then
-//    var a=20;
-//    var b=10;
-//    console.log(a>b);
- 
-// var name=50;
-// var name1=60;
-// console.log(name > name1);
-// var a=40;
-// var b=10;
-// console.log(a>b);
-
-// var c=30;
-// var d=20;
-// console.log(c==d);
+//  < less then 
+// var num=52;
+// var num1=12;
+//  console.log(num<num1);
 // var a=10;
-// var b=12;
-// console.log(a!=b);
+// var b=5;
+// console.log(a<b);
+// var a=20;
+// var b=25;
+// console.log(a<b);
+// var c=15.5;
+// var d=19;
+// console.log(c<d);
+// var a=50;
+// var b=60;
+// console.log(a<b);
+// var c=10.5;
+// var d=2;
+// console.log(c<d);
 
-// program
-// var userinput=+prompt("User Enter number");
-// var userinput1=+prompt("User Enter a number");
-//     if (userinput+userinput1 ||userinput-userinput1) {
-//     console.log("Result",userinput+userinput1);
-// }
-// else if (userinput-userinput1) {
-//  console.log('Result',userinput-userinput1);
-// }
+// // > Greater then
+// //    var a=20;
+// //    var b=10;
+// //    console.log(a>b);
+ 
+// // var name=50;
+// // var name1=60;
+// // console.log(name > name1);
+// // var a=40;
+// // var b=10;
+// // console.log(a>b);
 
-// function abc(number) {
-//     console.log("paksitan")
-// }
-// abc()
-// function sumtwonumber(a,b) {
-//     // var a=5;
-//     // var b=1;
-//     var a=prompt("enter number");
-// var b=prompt("enter number");
-//     console.log(a+b);
-// }
+// // var c=30;
+// // var d=20;
+// // console.log(c==d);
+// // var a=10;
+// // var b=12;
+// // console.log(a!=b);
 
-// sumtwonumber(a+b)
+// // program
+// // var userinput=+prompt("User Enter number");
+// // var userinput1=+prompt("User Enter a number");
+// //     if (userinput+userinput1 ||userinput-userinput1) {
+// //     console.log("Result",userinput+userinput1);
+// // }
+// // else if (userinput-userinput1) {
+// //  console.log('Result',userinput-userinput1);
+// // }
 
-// function addtwonumber(userinput,userinput1) {
-//     var result=+userinput+userinput1;
-//     console.log("result",result);
-// }
-// var userinput=+prompt("Entet Number");
-// var userinput1=+prompt("Enter Number");
-// addtwonumber(userinput,userinput1)
+// // function abc(number) {
+// //     console.log("paksitan")
+// // }
+// // abc()
+// // function sumtwonumber(a,b) {
+// //     // var a=5;
+// //     // var b=1;
+// //     var a=prompt("enter number");
+// // var b=prompt("enter number");
+// //     console.log(a+b);
+// // }
 
-// function abc(a,b){
-//     var sum1=+prompt("Enter User Number");
-//     var sum2=+prompt("Enter User Number");
-//     var result=sum1+sum2;
-//     console.log("result",result);
-// }
-// abc()
+// // sumtwonumber(a+b)
+
+function addtwonumber(userinput,userinput1) {
+    var result=+userinput+userinput1;
+    console.log("result",result);
+}
+var userinput=+prompt("Entet Number");
+var userinput1=+prompt("Enter Number");
+addtwonumber(userinput,userinput1)
+
+function abc(a,b){
+    var sum1=+prompt("Enter User Number");
+    var sum2=+prompt("Enter User Number");
+    var result=sum1+sum2;
+    console.log("result",result);
+}
+abc()
 // function abc(a,b){
 //     var age=+prompt("Enter user age")
 //     if (age>=1 && age<=15 ) {
